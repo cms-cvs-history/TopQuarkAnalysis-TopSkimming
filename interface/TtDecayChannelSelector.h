b5fe83386816ed5a98c82a30277ba28ceb68f0c5
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2007-08-02 12:03:33 +0000

    changed filter logic for dileptonic channel

--- interface/TtDecayChannelSelector.h
+++ interface/TtDecayChannelSelector.h
@@ -4,7 +4,8 @@
 
 class TtDecayChannelSelector {
  public:
-  enum Leaf
+
+  enum Index
     {
       Elec=0,
       Muon=1,
@@ -15,10 +16,15 @@ class TtDecayChannelSelector {
   TtDecayChannelSelector(const edm::ParameterSet&);
   ~TtDecayChannelSelector();
   bool operator()(const reco::CandidateCollection&) const;
+
  private:
-  int channel_;  //top decay channel
-  bool invert_;  //inversion flag
-  Decay chn1_;   //vector of allowed lepton decay channels for one W
-  Decay chn2_;   //vector of allowed lepton decay channels for the other W
-  Decay decay_;  //vector of allowed lepton decay channels for the other W
+
+  void parseDecayInput(Decay&, Decay&) const;
+
+ private:
+
+  bool  invert_;  //inversion flag
+  int   channel_; //top decay channel
+  int   summed_;
+  Decay decay_;
 };
